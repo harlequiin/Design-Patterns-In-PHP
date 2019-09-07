4143140488e0490baf8da0eed3c4a26b977a4d1d
Maris Udris <maris.udris.lv@gmail.com>
2019-09-07 09:23:28 +0300

    refactored the example - stripped service layer classes + tests

--- Domain Model/src/Repository/InvoiceRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace harlequiin\Patterns\DomainModel\Repository;
-
-interface InvoiceRepositoryInterface extends RepositoryInterface
-{
-}
--- Domain Model/src/Repository/OrderRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace harlequiin\Patterns\DomainModel\Repository;
-
-interface OrderRepositoryInterface extends RepositoryInterface
-{
-    public function getUninvoicedOrders();
-}
