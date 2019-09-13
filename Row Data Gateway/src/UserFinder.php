0bbf912602cb1317984570a1146e8ad6230c60a6
Maris Udris <maris.udris.lv@gmail.com>
2019-09-13 13:50:37 +0300

    completed the UserFinder class

---
+++ Row Data Gateway/src/UserFinder.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace harlequiin\Patterns\RowDataGateway;
+
+use harlequiin\Patterns\RowDataGateway\UserGateway;
+use PDO;
+
+class UserFinder
+{
+    /**
+     * @var PDO PHP Data Object
+     */
+    private $pdo;
+
+    /**
+     * @var string table name
+     */
+    private const TABLE = "user";
+    
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo; 
+    }
+
+    public function find(int $id): UserGateway
+    {
+        $sql = "SELECT * FROM " . self::TABLE . " WHERE `id` = :id); ";
+
+        try {
+            $pdoStatement = $this->pdo->prepare($sql);
+            $pdoStatement->execute([
+                "id" => $id
+            ]);
+
+            $pdoObject = $pdoStatement->fetch(PDO::FETCH_OBJ);
+
+            $gateway = new UserGateway($this->pdo);
+            $gateway->setId($pdoObject->id);
+            $gateway->setUsername($pdoObject->username);
+            $gateway->setAge($pdoObject->age);
+
+            return $gateway;
+
+        } catch (\Exception $e) {
+            throw new GatewayException($e->getMessage());
+        }
+    }
+
+    public function findByUsername(string $username): UserGateway
+    {
+        $sql = "SELECT * FROM " . self::TABLE . " WHERE `username` = :username); ";
+
+        try {
+            $pdoStatement = $this->pdo->prepare($sql);
+            $pdoStatement->execute([
+                "username" => $username
+            ]);
+
+            $pdoObject = $pdoStatement->fetch(PDO::FETCH_OBJ);
+
+            $gateway = new UserGateway($this->pdo);
+            $gateway->setId($pdoObject->id);
+            $gateway->setUsername($pdoObject->username);
+            $gateway->setAge($pdoObject->age);
+
+            return $gateway;
+
+        } catch (\Exception $e) {
+            throw new GatewayException($e->getMessage());
+        }
+    }
+}
+
