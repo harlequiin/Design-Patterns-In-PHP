d16b7404715f409beaf0377206fd3414d4e03e20
Maris Udris <maris.udris.lv@gmail.com>
2020-01-03 23:23:42 +0200

    Added custom GatewayException class

---
+++ Table Data Gateway/src/GatewayException.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace harlequiin\Patterns\TableDataGateway;
+
+use Exception;
+
+/**
+ * GatewayException.
+ *
+ * A custom exception class for our gateways.
+ */
+class GatewayException extends Exception
+{}
